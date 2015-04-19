c2995d5c1c14326d5a4ff6e785685742f5ed49a3
reachfrancis@verizon.net <reachfrancis@verizon.net>
2015-04-19 14:58:43 -0400

    Added a test route...

--- app/app.js
+++ app/app.js
@@ -12,63 +12,7 @@
 //  Enclose the application  Use closure since its good Javascript practice.
 (function () {
 
-    'use strict';
+    var valverdeCorpApp = angular.module('valCorp', []);
 
-    var  valverdeCorpApp  = angular.module('valCorp',
-        [ 'ngRoute',
-            'ngAnimate',
-            'ui.bootstrap',
-            'ui.bootstrap.tooltip',
-            'HomePageModController'
-        ]);
-
-
-
-
-
-    // Enable / disable $log.debug output.
-    valverdeCorpApp.config(function ($logProvider) {
-        $logProvider.debugEnabled(true);
-    });
-
-
-
-
-    // Routing ==============================================================================================
-    // setup our routing for this application
-    // Remember the Information  Guides are handled via the common/winPopUpService.js  service.
-    // ======================================================================================================
-
-    valverdeCorpApp.config(function ($routeProvider) {
-        $routeProvider.
-            when('/home', {
-                templateUrl: 'app/Home/_HomePagePartialView.html',
-                controller: 'HomePageController',
-                caseInsensitiveMatch:true
-            }).
-
-            when('/services', {
-
-            }).
-
-            when('/test', {}).
-
-            when ('/contact-us',{
-
-              }).
-
-            when ('/register', {}).
-
-            when('about-us', {}).
-
-            when('blog', {}).
-
-            otherwise({
-                redirectTo: '/home'
-            });
-    });
-
-
-
-})();
+}());
 
