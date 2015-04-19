728b9b482a6bf65ae5d721cea5ed235542862a46
reachfrancis@verizon.net <reachfrancis@verizon.net>
2015-04-18 20:53:00 -0400

    initial project Version

---
+++ app/MasterLayout/headerMenu/headerMenuController.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Francis on 4/18/2015.
+ */
---
+++ app/app.js
@@ -0,0 +1,72 @@
+
+/* Authors..:Francis Contreras
+             David Jones
+*/
+
+
+
+/// <reference path="/app/library/google/js/angular.js" />
+
+"use strict";
+
+//  Enclose the application  Use closure since its good Javascript practice.
+(function () {
+
+    'use strict';
+
+    var  valverdeCorpApp  = angular.module('valverde_Corp',
+        [ 'ngRoute',
+            'ngAnimate',
+            'ui.bootstrap',
+            'ui.bootstrap.tooltip',
+            'HomePageModController'
+        ]);
+
+
+
+
+
+    // Enable / disable $log.debug output.
+    valverdeCorpApp.config(function ($logProvider) {
+        $logProvider.debugEnabled(true);
+    });
+
+
+
+
+    // Routing ==============================================================================================
+    // setup our routing for this application
+    // Remember the Information  Guides are handled via the common/winPopUpService.js  service.
+    // ======================================================================================================
+
+    valverdeCorpApp.config(function ($routeProvider) {
+        $routeProvider.
+            when('/home', {
+                templateUrl: 'app/Home/_HomePagePartialView.html',
+                controller: 'HomePageController',
+                caseInsensitiveMatch:true
+            }).
+
+            when('/services', {
+
+            }).
+
+            when ('/contact-us',{
+
+              }).
+
+            when ('/register', {}).
+
+            when('about-us', {}).
+
+            when('blog', {}).
+
+            otherwise({
+                redirectTo: '/home'
+            });
+    });
+
+
+
+})();
+
